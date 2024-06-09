3cc74b309180f9dc29dace7f005591474103bfab
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2024-06-09 16:04:01 +0000

    Clean up stuff

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-import { HamburgerMenuIcon, InstagramLogoIcon } from "@radix-ui/react-icons";
 import type {
   LinksFunction,
   LoaderFunctionArgs,
@@ -6,35 +5,16 @@ import type {
 } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
-  Form,
-  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useSubmit,
 } from "@remix-run/react";
-import { useRef } from "react";
 
 import stylesheet from "~/tailwind.css?url";
-import { getUserWithRole } from "~/utils/session.server";
 
-import { Footer } from "./components/footer";
-import { Logo } from "./components/logo";
-import { Button } from "./components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuPortal,
-  DropdownMenuTrigger,
-} from "./components/ui/dropdown-menu";
-import { Toaster } from "./components/ui/sonner";
-import { useToast } from "./hooks/useToast";
-import { combineHeaders, getDomainUrl, useOptionalUser } from "./utils/misc";
-import { getToast } from "./utils/toast.server";
+import { getDomainUrl } from "./utils/misc";
 
 export type RootLoaderData = SerializeFrom<typeof loader>;
 
@@ -72,9 +52,7 @@ export const links: LinksFunction = () => [
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const domainUrl = getDomainUrl(request);
-  const user = await getUserWithRole(request);
-  const { toast, headers } = await getToast(request);
-  return json({ user, toast, domainUrl }, { headers: combineHeaders(headers) });
+  return json({ domainUrl });
 };
 
 export default function App() {
@@ -86,154 +64,19 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="h-full bg-gray-950 text-gray-50">
+      <body className="h-full">
         <Document />
         <ScrollRestoration />
         <Scripts />
-        <Toaster />
       </body>
     </html>
   );
 }
 
 function Document() {
-  const optionalUser = useOptionalUser();
-  const { toast } = useLoaderData<typeof loader>();
-  useToast(toast);
-
   return (
     <>
-      <nav className="h-20 border-b border-gray-50 bg-gray-950 text-gray-50">
-        <div className="mx-auto flex h-full max-w-4xl flex-wrap items-center justify-between gap-4 px-2 sm:flex-nowrap md:gap-8">
-          <Link to="/" className="flex items-center gap-6 font-bold">
-            <Logo className="h-6 w-6" />
-            moku pona
-          </Link>
-
-          <div className="flex items-center gap-10 max-md:gap-5">
-            <Link to="/dinners" className="hover:underline max-md:hidden">
-              upcoming dinners
-            </Link>
-
-            <Link
-              to="/about"
-              className="pointer-events-none text-foreground/20 max-md:hidden"
-            >
-              about
-            </Link>
-
-            <a
-              href="https://instagram.com/apsysikal"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-1 hover:underline max-md:hidden"
-            >
-              <InstagramLogoIcon className="h-6 w-6" />
-            </a>
-
-            {["moderator", "admin"].includes(optionalUser?.role.name ?? "") ? (
-              <Link
-                prefetch="intent"
-                to="/admin"
-                className="hover:underline max-md:hidden"
-              >
-                admin area
-              </Link>
-            ) : null}
-
-            {optionalUser ? (
-              <Form action="/logout" method="POST" className="max-md:hidden">
-                <button className="hover:underline ">logout</button>
-              </Form>
-            ) : (
-              <Link to="/login" className="hover:underline max-md:hidden">
-                login
-              </Link>
-            )}
-
-            <span className="md:hidden">
-              <GeneralDropdown />
-            </span>
-          </div>
-        </div>
-      </nav>
       <Outlet />
-      <Footer />
     </>
-  );
-}
-
-function GeneralDropdown() {
-  const optionalUser = useOptionalUser();
-  const submit = useSubmit();
-  const formRef = useRef<HTMLFormElement>(null);
-
-  return (
-    <DropdownMenu modal={false}>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="text-body.sm">
-          <HamburgerMenuIcon />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuPortal>
-        <DropdownMenuContent sideOffset={8} align="start">
-          <DropdownMenuItem>
-            <Link to="/dinners" className="hover:underline">
-              upcoming dinners
-            </Link>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem>
-            <Link
-              to="/about"
-              className="pointer-events-none text-foreground/20"
-            >
-              about
-            </Link>
-          </DropdownMenuItem>
-
-          <DropdownMenuItem>
-            <a
-              href="https://instagram.com/apsysikal"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-1 hover:underline"
-            >
-              <InstagramLogoIcon className="h-6 w-6" />
-              instagram
-            </a>
-          </DropdownMenuItem>
-
-          {["moderator", "admin"].includes(optionalUser?.role.name ?? "") ? (
-            <DropdownMenuItem asChild>
-              <Link prefetch="intent" to="/admin">
-                admin area
-              </Link>
-            </DropdownMenuItem>
-          ) : null}
-
-          {optionalUser ? (
-            <DropdownMenuItem
-              asChild
-              // this prevents the menu from closing before the form submission is completed
-              onSelect={(event) => {
-                event.preventDefault();
-                submit(formRef.current);
-              }}
-            >
-              <Form action="/logout" method="POST" ref={formRef}>
-                <button type="submit">logout</button>
-              </Form>
-            </DropdownMenuItem>
-          ) : (
-            <DropdownMenuItem asChild>
-              <Link prefetch="intent" to="/login">
-                login
-              </Link>
-            </DropdownMenuItem>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenuPortal>
-    </DropdownMenu>
   );
 }
--- app/utils/address-validation.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const AddressSchema = z.object({
-  streetName: z.string({ required_error: "Street Name is required" }).trim(),
-  houseNumber: z.string({ required_error: "House Number is required" }).trim(),
-  zipCode: z.string({ required_error: "Zip Code is required" }).trim(),
-  city: z.string({ required_error: "City is required" }).trim(),
-});
